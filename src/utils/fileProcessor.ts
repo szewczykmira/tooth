a6b3eac63b0b049190f7bfc2f702fc972960b630
Mira <6984271+szewczykmira@users.noreply.github.com>
2024-12-15 21:19:06 +0100

    Read csv

--- src/utils/fileProcessor.ts
+++ src/utils/fileProcessor.ts
@@ -1,6 +1,19 @@
+import Papa from "papaparse";
+const readContent = (fslData: File) => {
+  const fileReader = new FileReader();
+  fileReader.onload = () => {
+    const fileContent = fileReader.result as string;
+    const { data } = Papa.parse(fileContent);
+    console.log(data);
+  };
+  fileReader.readAsText(fslData);
+};
+
 export const fileProcessor = (fslData: File | null) => {
   if (!fslData || fslData?.size === 0) {
     alert("sorry! this is not a valid file!");
+    return [];
   }
+  readContent(fslData);
   return [];
 };
