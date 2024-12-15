012dbdc48d7b80040a990dd63b7789a12dccec84
Mira <6984271+szewczykmira@users.noreply.github.com>
2024-12-15 22:06:30 +0100

    Return processed data

--- src/utils/fileProcessor.ts
+++ src/utils/fileProcessor.ts
@@ -1,19 +1,40 @@
 import Papa from "papaparse";
-const readContent = (fslData: File) => {
-  const fileReader = new FileReader();
-  fileReader.onload = () => {
-    const fileContent = fileReader.result as string;
-    const { data } = Papa.parse(fileContent);
-    console.log(data);
-  };
-  fileReader.readAsText(fslData);
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+
+dayjs.extend(customParseFormat);
+
+export type FSLResult = {
+  date: Date;
+  value: number;
 };
 
-export const fileProcessor = (fslData: File | null) => {
+const readContent = (fslData: File): Promise<FSLResult[]> => {
+  return new Promise((res) => {
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      const fileContent = fileReader.result as string;
+      const { data } = Papa.parse(fileContent);
+
+      res(
+        (data.slice(2) as string[6][]).map(
+          (item): FSLResult => ({
+            date: dayjs(item[2], "DD-MM-YYYY HH:mm").toDate(),
+            value: Number(item[4]),
+          }),
+        ),
+      );
+    };
+    fileReader.readAsText(fslData);
+  });
+};
+
+export const fileProcessor = async (
+  fslData: File | null,
+): Promise<FSLResult[]> => {
   if (!fslData || fslData?.size === 0) {
     alert("Sorry! this is not a valid file!");
     return [];
   }
-  readContent(fslData);
-  return [];
+  return await readContent(fslData);
 };
