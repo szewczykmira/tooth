27a10e1517eee7dc57e8cc81cf4af0d9b252a40a
Mira <6984271+szewczykmira@users.noreply.github.com>
2025-06-03 21:32:51 +0200

    Display chart only if data were provided

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import { Form } from "../Form/Form";
 import { FSLResult } from "../../utils/fileProcessor";
+import { Chart } from "../Chart/Chart";
 
 export const Body = () => {
   const [fslData, setFslData] = useState<FSLResult[]>([]);
-  return <>{fslData.length === 0 && <Form storeData={setFslData} />}</>;
+
+  return (
+    <>
+      {fslData.length === 0 ? (
+        <Form storeData={setFslData} />
+      ) : (
+        <Chart data={fslData} />
+      )}
+    </>
+  );
 };
