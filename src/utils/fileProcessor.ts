4f9f2887e6cbe17e2c0dbe619436151bac71bc91
Mira <6984271+szewczykmira@users.noreply.github.com>
2025-06-03 21:42:21 +0200

    Filter out empty values

--- src/utils/fileProcessor.ts
+++ src/utils/fileProcessor.ts
@@ -9,20 +9,24 @@ export type FSLResult = {
   value: number;
 };
 
+const fetchData = (item: string): FSLResult => {
+  const result = Number(item[4] || item[5] || 0);
+  return {
+    date: dayjs(item[2], "DD-MM-YYYY HH:mm").toDate(),
+    value: result,
+  };
+};
+
 const readContent = (fslData: File): Promise<FSLResult[]> => {
   return new Promise((res) => {
     const fileReader = new FileReader();
     fileReader.onload = () => {
       const fileContent = fileReader.result as string;
       const { data } = Papa.parse(fileContent);
-
       res(
-        (data.slice(2) as string[6][]).map(
-          (item): FSLResult => ({
-            date: dayjs(item[2], "DD-MM-YYYY HH:mm").toDate(),
-            value: Number(item[4]),
-          }),
-        ),
+        (data.slice(2) as string[6][])
+          .map(fetchData)
+          .filter(({ value }: FSLResult) => value > 0),
       );
     };
     fileReader.readAsText(fslData);
